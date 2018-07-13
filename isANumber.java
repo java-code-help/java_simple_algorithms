bf77c2aeff5cdc880e75305aaf29eed0d334023f
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-07-13 22:55:44 +0300

    Update isANumber.java

--- isANumber.java
+++ isANumber.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.CodeFights;
 
 public class IsANumber {
     static boolean isNumeric(String arg){
@@ -9,4 +9,27 @@ public class IsANumber {
         }
         return isChar;
     }
+    static int lengthOfInteger(Integer integer){
+        int numerator = 0;
+        for (Character i:integer.toString().toCharArray()){
+            numerator++;
+        }
+        return numerator;
+    }
+
+    public static void main(String[] args) {
+        int tribulus = 2234242;
+        int lenStringParsing = Integer.toString(tribulus).length(); // first method to find integer length
+        int len = (int) (Math.log10(tribulus)+1); // second method
+        int lenghtMethod = lengthOfInteger(tribulus);
+
+        System.out.println(
+                lenghtMethod +
+                " <- from method " + "\n" +
+                        "-> from log10 method " +
+                len + "\n" + " -> from parsed String = " +
+                lenStringParsing); //third method
+    }
+
 }
+
