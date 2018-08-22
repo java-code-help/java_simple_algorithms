ecdc2ece1987b1079bfdeb2f8e04cebe0bb86fc9
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-08-22 12:09:52 +0300

    Create GuessGame.java

---
+++ GuessGame.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CleanCode {
+    private boolean StringToChars(String param) {
+        Scanner sc = new Scanner(System.in);
+        boolean found = false;
+        char[] missingLetters = new char[param.length()];
+        int d = Integer.MAX_VALUE;
+        char[] buffer = param.toCharArray();
+        char[] chars = new char[buffer.length];
+        d = chars.length;
+        System.out.println("Can you guess the movie ? " + " \n" + "hint: " +
+        param.length() + " letters");
+        String c = sc.nextLine();
+        char[] Cbuffer = c.toCharArray();
+        char[] Cchars = new char[param.length()];
+        for (int x=0;x<Cchars.length;x++) {
+            Cchars[x] = Cbuffer[x];
+        }
+        System.out.println("each char: ");
+        for (int i=0;i<chars.length;i++) {
+            chars[i] = buffer[i];
+
+
+            if(chars[i] == Cchars[i]) {
+                d--;
+                System.out.print(Cchars[i] + " ");
+            } else if (chars[i] != Cchars[i]){
+                System.out.print(" _ ");
+                missingLetters[i] = chars[i];
+                found = false;
+            }
+
+
+
+            found = (d == 0) ? true : false;
+
+
+            if (found){System.out.println("100% found WOW!");}
+
+
+        }
+        System.out.print("missing letters: " + Arrays.toString(missingLetters));
+        System.out.println("\nremained " + d + " letters unfound");
+//        System.out.println(Arrays.toString(chars));
+
+        return found;
+    }
+
+    public static void main(String[] args) {
+        CleanCode cleanCode = new CleanCode();
+        cleanCode.StringToChars("abcdefg");
+
+    }
+}
