7e00e8c6822f68d8d202f69f05e05c4ddc424705
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-09-17 11:40:05 +0300

    Update isMAC48Address.java

--- isMAC48Address.java
+++ isMAC48Address.java
@@ -2,16 +2,31 @@ static boolean isMac48(String input){
         boolean condition = false;
         String regex = "[A-Fa-f0-9]+";
         Pattern pattern = Pattern.compile(regex);
-        String[] toCheck = input.split("-");
-        for (int i = 0; i < toCheck.length; i++) {
-            if (!pattern.matcher(toCheck[i]).lookingAt()){
-                condition = false;
-                System.out.println("false here " + toCheck[i]);
-                return condition;
-            } else {
-                condition = true;
-                System.out.println("true here "+toCheck[i]);
+        if (input.length()<18) {
+            String[] toCheck = input.split("-");
+            System.out.println(Arrays.toString(toCheck));
+            for (int i = 0; i < toCheck.length; i++) {
+//            if (!pattern.matcher(toCheck[i]).lookingAt()){
+                for (Character c : toCheck[i].toCharArray()) {
+                    if (!pattern.matcher(c.toString()).lookingAt()) {
+                        condition = false;
+                        System.out.println("false inner");
+                        return condition;
+                    } else {
+                        condition = true;
+                        System.out.println("true here " + toCheck[i]);
+                    }
+                }
+//                condition = false;
+//                System.out.println("false here " + toCheck[i]);
+//                return condition;
+//            } else {
+//                condition = true;
+//                System.out.println("true here "+toCheck[i]);
+//            }
             }
+        } else {
+            condition=false;
         }
         return condition;
     }
