84eec622decfb6cc3fb827d5e58be9e993ad84f8
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-09-17 16:01:32 +0300

    Update countCharOccurence.java

--- countCharOccurence.java
+++ countCharOccurence.java
@@ -1,3 +1,4 @@
+//counter SAME chars
 public static int countChar(String str, char ch){
         int count=0;
         while (str.indexOf(ch)!=-1){
@@ -13,6 +14,23 @@ public static int countChar(String str, char ch){
            int count = countChar(str, ch);
            System.out.println(ch + " " + count);
            str = str.replace(""+ch,"");
-
        }
+    }
+//counter EACH char
+static String lineEncoding(String s) {
+        s += "#";
+        int cnt = 1;
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                cnt++;
+            } else {
+                if (cnt > 1) {
+                    result.append(cnt);
+                }
+                result.append(s.charAt(i-1));
+                cnt=1;
+            }
+        }
+        return result.toString();
     }
