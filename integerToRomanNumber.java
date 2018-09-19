379e2d6f4b41c0f0932fe8bb87e507335cedf590
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-09-19 10:50:59 +0300

    Update integerToRomanNumber.java

--- integerToRomanNumber.java
+++ integerToRomanNumber.java
@@ -44,3 +44,47 @@ String s = "";
 
     return s;
 }
+
+
+//js
+rome = n =>  {
+    c = 'IVXLCDM'
+    for (a = 5, b = s = ''; n; b++, a ^= 7)
+        for (o = n % a, n = n / a ^ 0; o--; s = c[~~(o > 2 ? b + n - (n &= -2) + (o = 1) : b)] + s) ;
+    return s
+}
+//py2
+def rome(n):
+    a = ""
+    i = 0
+    N = [1000,900,500,400]
+    for k in N:
+        if k:
+            a += n/k*(i%2*'CXI '[i/4]+'MDCLXVI'[i/2])
+            n %= k  
+            N += k/10,
+            i += 1
+
+    return a
+//haskell
+
+rome = f 0
+
+f i 0                       = ""
+f i n
+    | t <- mod n 10, t > 0  = f i (n - y t) ++ ["IVXLCDM" !! (i + x t)]
+    | 1 > 0                 = f (i+2) $ div n 10
+
+x 4 = 1
+x 5 = 1
+x 9 = 2
+x _ = 0
+
+y 4 = 3
+y 5 = 5
+y 9 = 8
+y _ = 1
+
+//lisp
+    (defun rome (n) 
+  (format / "~@r" n))
