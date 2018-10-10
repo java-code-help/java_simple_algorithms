a23891d0d6c01d21c837e2db1d8e82dc48cdd1e1
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-10-10 11:36:21 +0300

    Update sumUpNumbers.java

--- sumUpNumbers.java
+++ sumUpNumbers.java
@@ -6,6 +6,14 @@ private static int sumUpNumbers(String inputString) {
             cost+=Integer.parseInt(m.group());
             System.out.println(m.group());
         }
+        
+        //variant #2
+//         String[] splittingTheNumbers=inputString.split("\\D+");
+//         int count=0;
+//         for (String num:splittingTheNumbers){
+//             if (num.length()>0) count+=Integer.parseInt(num);
+//         }
+//         return count;
 
         System.out.println(cost);
         return cost;
